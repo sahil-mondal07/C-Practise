b0aa7a5eaf9e1177937cbb11cb8618c34fe8614b
sahil-mondal07 <mondalsahil591@gmail.com>
2026-08-12 18:35:35 +0530

    q1

--- q1.c
+++ q1.c
@@ -2,11 +2,29 @@
 /*Input : 854102967
 Output:  3*/
 
-#include<stdio.h>
-
-int main(){
+#include <stdio.h>
+int main() {
     int num;
-    printf("enter a num: ")
-    scanf("%d",&num);
-    
+    printf("Enter a number: ");
+    scanf("%d", &num);
+    for (int i = 0; i <= 9; i++) {
+        int temp = num;
+        int found = 0;
+        while (temp > 0) {
+
+            int e = temp % 10;
+
+            if (e == i) {
+                found = 1;
+                break;
+            }
+
+            temp = temp / 10;
+        }
+        if (found == 0) {
+            printf("Missing digit: %d\n", i);
+            break;
+        }
+    }
+    return 0;
 }
