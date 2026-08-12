85e3735603f1718db37dc18e36eeb1b60e432064
sahil-mondal07 <mondalsahil591@gmail.com>
2026-08-12 17:35:00 +0530

    Update q1.c and add q2.c

--- q1.c
+++ q1.c
@@ -3,25 +3,10 @@
 Output:  3*/
 
 #include<stdio.h>
+
 int main(){
     int num;
-    printf("enter a num : ");
+    printf("enter a num: ")
     scanf("%d",&num);
-    for(int i =0;i<=9;i++){
-        int temp = num;
-        int a=0;
-        while(temp>0){
-            int e=temp%10;
-            if(e==i){
-                a=1;
-                break;
-            }
-            temp=temp/10;
-        }
-        if(a==0){
-            printf("missing digit : %d ",i);
-            break;
-        }
-    }
-    return 0;
+    
 }
---
+++ q2.c
@@ -0,0 +1,17 @@
+/*Write a program using a while loop that repeatedly asks the user to enter the password and stops only when the correct password is entered. Finally, display "Login successful!"*/
+
+#include<stdio.h>
+#include <string.h>
+int main(){
+    char apass[100];
+    char pass[]="sahil007";
+    while(1>0){
+        printf("enter pass : ");
+        scanf("%s",apass);
+        if(strcmp(apass, pass) == 0){
+            printf("login succesfull");
+            break;
+        }
+    }
+    return 0;
+}
